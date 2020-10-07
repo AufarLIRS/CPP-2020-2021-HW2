3da3a52f9e08da33bf3a17484bbfabd3f9f1f373
ZeByR0 <mezin.2001@gmail.com>
2020-10-07 18:39:14 +0300

    HW by Igor Mezin

--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_submitPushButton_clicked();
+
+    void on_add_button_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
