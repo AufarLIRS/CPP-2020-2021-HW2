a28ad8f74365b8dab83b82cfed0e730abcb17618
CamiloFajardo200 <camilo.fajardo.2001@hotmail.com>
2020-10-07 13:48:19 +0300

    HW2
    I Have a problem when deleting a random student , it always deletes the last one and i dont understand why . I added the print button for debuging

--- mainwindow.h
+++ mainwindow.h
@@ -7,6 +7,8 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -14,6 +16,10 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_AddButton_clicked();
+
 
 private:
     Ui::MainWindow *ui;
