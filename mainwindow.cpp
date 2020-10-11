9550925fd3ba3d32476f90397c01317a987e9b79
APGDiamond <shalmaz1733@gmail.com>
2020-10-11 22:01:54 +0300

    HW fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,117 +6,114 @@
 
 class User
 {
-  int age_;
-  QString name_;
-
+    int age_;
+    QString name_;
 public:
-  explicit User(QString name, int age = 18)
-  {
-    if (age >= 18)
+    explicit User(QString name, int age = 18)
     {
-      this->age_ = age;
+        if (age >= 18)
+        {
+            this->age_ = age;
+        }
+        else
+        {
+            this->age_ = 18;
+        }
+        this->name_ = name;
     }
-    else
+    static void addUser(User user)
     {
-      this->age_ = 18;
+        users.push_back(user);
     }
-    this->name_ = name;
-  }
-  static void addUser(User user)
-  {
-    users.push_back(user);
-  }
-  static void addUser(QString name, int age = 18)
-  {
-    User user(name, age);
-    addUser(user);
-  }
-  static bool removeUser(unsigned long index)
-  {
-    if (index < getTotalCount())
+    static void addUser(QString name, int age = 18)
     {
-      users.erase(users.begin() + index);
-      return true;
+        User user(name, age);
+        addUser(user);
     }
-    return false;
-  }
-  static unsigned long removeUser(QString name)
-  {
-    int erased = 0;
-    for (auto i = users.begin(); i < users.end(); i++)
+    static bool removeUser(unsigned long index)
     {
-      if (i->name_ == name)
-      {
-        users.erase(i);
-        erased++;
-      }
+        if (index < getTotalCount())
+        {
+            users.erase(users.begin() + index);
+            return true;
+        }
+        return false;
     }
-    return erased;
-  }
-  QString getName()
-  {
-    return name_;
-  }
-  int getAge()
-  {
-    return age_;
-  }
-  static unsigned long getTotalCount()
-  {
-    return users.size();
-  }
-  static std::vector<User> users;
+    static unsigned long removeUser(QString name)
+    {
+        int erased = 0;
+        for (auto i = users.begin(); i < users.end(); i++)
+        {
+            if (i->name_ == name)
+            {
+                users.erase(i);
+                erased++;
+            }
+        }
+        return erased;
+    }
+    static unsigned long getTotalCount()
+    {
+        return users.size();
+    }
+private:
+    QString getName()
+    {
+        return name_;
+    }
+    int getAge()
+    {
+        return age_;
+    }
+    static std::vector<User> users;
 };
 
 std::vector<User> User::users;
 
 std::string randString()
 {
-  std::string tmp = "";
-  static const char a[] = "abcdefghijklmnopqrstuvwxyz";
-  int l = rand() % 25 + 1;
-  for (int i = 0; i < l; ++i)
-  {
-    tmp += a[rand() % (sizeof(a) - 1)];
-  }
-  return tmp;
+    std::string tmp = "";
+    static const char a[] = "abcdefghijklmnopqrstuvwxyz";
+    int l = rand() % 25 + 1;
+    for (int i = 0; i < l; ++i)
+    {
+        tmp += a[rand() % (sizeof(a) - 1)];
+    }
+    return tmp;
 }
 
 void MainWindow::on_addButton_clicked()
 {
-  User::addUser(QString::fromStdString(randString()), rand() % 63 + 18);
-  // qDebug() << User::getTotalCount();
+    User::addUser(QString::fromStdString(randString()), rand() % 63 + 18);\
 }
 
 void onDeleteClick()
 {
-  if (User::getTotalCount() > 0)
-  {
-    User::removeUser(rand() % User::getTotalCount());
-  }
-  // qDebug() << User::getTotalCount();
+    if (User::getTotalCount() > 0)
+    {
+        User::removeUser(rand() % User::getTotalCount());
+    }
 }
 
 void onDeleteAllClick()
 {
-  unsigned long i = User::getTotalCount();
-  while (i > 0)
-  {
-    i--;
-    User::removeUser(i);
-  }
-  // qDebug() << User::getTotalCount();
+    unsigned long i = User::getTotalCount();
+    while (i > 0)
+    {
+        i--;
+        User::removeUser(i);
+    }
 }
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
-  srand(time(NULL));
-  ui->setupUi(this);
-  connect(ui->deleteButton, &QPushButton::clicked, this, &onDeleteClick);
-  connect(ui->deleteAllButton, &QPushButton::clicked, this, &onDeleteAllClick);
+    srand(time(NULL));
+    ui->setupUi(this);
+    connect(ui->deleteButton, &QPushButton::clicked, this, &onDeleteClick);
+    connect(ui->deleteAllButton, &QPushButton::clicked, this, &onDeleteAllClick);
 }
 
 MainWindow::~MainWindow()
 {
-  delete ui;
+    delete ui;
 }
